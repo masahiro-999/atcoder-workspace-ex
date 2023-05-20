0b34140508f8697167a4d3acfa59859fd7d174ac
masahiro-999 <masahiro999@outlook.com>
2023-05-20 10:12:19 +0900

    update template

--- my_template.ex
+++ my_template.ex
@@ -1,6 +1,14 @@
 defmodule Main do
+    import Bitwise
+    def next_token(acc \\ "") do
+        case IO.getn(:stdio, "", 1) do
+          " " -> acc
+          "\n" -> acc
+          x -> next_token(acc <> x)
+        end
+    end
     def input(), do: IO.read(:line) |> String.trim()
-    def ii(), do: input() |> String.to_integer()
+    def ii(), do: next_token() |> String.to_integer()
     def li(), do: input() |> String.split(" ") |> Enum.map(&String.to_integer/1)
 
 {% if mod %}
