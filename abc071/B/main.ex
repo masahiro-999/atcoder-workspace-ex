24fd53e6b8a87ad075fe57051e07b9b6b822aa96
masahiro-999 <masahiro999@outlook.com>
2023-05-27 06:41:43 +0900

    abc071_b

---
+++ abc071/B/main.ex
@@ -0,0 +1,36 @@
+defmodule Main do
+    import Bitwise
+    def next_token(acc \\ "") do
+        case IO.getn(:stdio, "", 1) do
+          " " -> acc
+          "\n" -> acc
+          x -> next_token(acc <> x)
+        end
+    end
+    def input(), do: IO.read(:line) |> String.trim()
+    def ii(), do: next_token() |> String.to_integer()
+    def li(), do: input() |> String.split(" ") |> Enum.map(&String.to_integer/1)
+
+    def ans(count, i) when i == ?z+1 do
+      "None"
+    end
+    def ans(count, i) do
+      cond do
+        not Map.has_key?(count, i) -> List.to_string([i])
+        :true -> ans(count, i+1)
+      end
+    end
+
+    def solve(s) do
+        String.to_charlist(s)
+        |> Enum.reduce(%{}, fn value, m -> Map.update(m, value, 0, &(&1+1))end)
+        |> ans(?a)
+        |> IO.puts()
+    end
+
+    def main() do
+        s = input()
+        solve(s)
+    end
+
+end
