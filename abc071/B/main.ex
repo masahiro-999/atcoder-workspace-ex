662123cd52bef410b44278d63e33be97fdfde349
masahiro-999 <masahiro999@outlook.com>
2023-05-27 12:15:52 +0900

    abc071_b

--- abc071/B/main.ex
+++ abc071/B/main.ex
@@ -11,15 +11,9 @@ defmodule Main do
     def ii(), do: next_token() |> String.to_integer()
     def li(), do: input() |> String.split(" ") |> Enum.map(&String.to_integer/1)
 
-    def ans(count, i) when i == ?z+1 do
-      "None"
-    end
-    def ans(count, i) do
-      cond do
-        not Map.has_key?(count, i) -> List.to_string([i])
-        :true -> ans(count, i+1)
-      end
-    end
+    def ans(_count, i) when i == ?z+1, do: "None"
+    def ans(count, i) when is_map_key(count, i), do: ans(count, i+1)
+    def ans(_count, i), do: List.to_string([i])
 
     def solve(s) do
         String.to_charlist(s)
