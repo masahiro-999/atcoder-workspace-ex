52716d426466cba6fa826273493284604b7cc473
masahiro-999 <masahiro999@outlook.com>
2023-06-16 19:31:35 +0900

    abc053_b

---
+++ abc053/B/main.ex
@@ -0,0 +1,34 @@
+defmodule Main do
+    import Bitwise
+    def next_token(acc \\ "") do
+        case IO.getn(:stdio, "", 1) do
+          " " -> acc
+          "\n" -> acc
+          x -> next_token(acc <> x)
+        end
+    end
+    def input(), do: IO.read(:line) |> String.trim()
+    def ii(), do: next_token() |> String.to_integer()
+    def li(), do: input() |> String.split(" ") |> Enum.map(&String.to_integer/1)
+
+    def find(s,c) do
+        s
+        |> String.to_charlist()
+        |> Enum.with_index()
+        |> Enum.find(nil, fn {v,i} -> v == c end)
+        |> elem(1)
+    end
+
+    def solve(s) do
+        start_pos = find(s,?A)
+        end_pos =String.length(s) - 1 - find(String.reverse(s), ?Z)
+        ans = end_pos - start_pos + 1
+        IO.puts(ans)
+    end
+
+    def main() do
+        s = input()
+        solve(s)
+    end
+
+end
