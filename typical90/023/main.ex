8c770c1d6d8386fb6d6b3015ec7dce5e986ec96d
masahiro-999 <masahiro999@outlook.com>
2023-07-05 22:15:05 +0900

    023 in_2.txt wa

--- typical90/023/main.ex
+++ typical90/023/main.ex
@@ -1,5 +1,5 @@
 defmodule Main do
-    import Bitwise
+    use Bitwise
     def next_token(acc \\ "") do
         case IO.getn(:stdio, "", 1) do
           " " -> acc
@@ -11,14 +11,64 @@ defmodule Main do
     def ii(), do: next_token() |> String.to_integer()
     def li(), do: input() |> String.split(" ") |> Enum.map(&String.to_integer/1)
 
-    MOD = 1000000007
+    # MOD = 1000000007
 
-    def solve() do
+    def is_white(c,i,j) do
+      String.slice(elem(c,i),j,1)=="."
+    end
 
+    def is_all_white(h, w, c, i, w_bit) do
+        0..w-1
+        |> Enum.map(fn x ->
+            (w_bit &&& (1 <<< x)) != 0 and
+             not is_white(c,i,x) end)
+        |> Enum.all?(fn x -> x == :false end)
+    end
+
+    def is_valid(bit1, bit2) do
+        (bit1 &&& bit2) == 0 and ((bit1 <<<1) &&& bit2) == 0 and ((bit2 <<<1) &&& bit1) == 0
+    end
+
+    def get_ans(h, w, c, 0, w_bit) do
+        # IO.inspect({0,w_bit})
+        if(
+         ((w_bit <<<1) &&& w_bit) == 0 and
+         is_all_white(h, w, c, 0, w_bit)) do
+            1
+         else
+            0
+         end
+        #  |> IO.inspect()
+    end
+
+    def get_ans(h, w, c, i, w_bit) do
+        # IO.inspect({i,w_bit})
+        if is_all_white(h, w, c, i, w_bit) do
+            0..(1<<<w)-1
+            |> Enum.reduce(0, fn
+                x, acc -> acc +
+                    if is_valid(x, w_bit) do
+                        get_ans(h, w, c, i-1, x)
+                    else
+                        0
+                    end
+                end)
+        else
+            0
+        end
+        # |> IO.inspect()
+    end
+
+    def solve(h,w,c) do
+        ans = get_ans(h, w, c, h, 0)
+        IO.puts(ans)
     end
 
     def main() do
-        # Failed to predict input format
+        h = ii()
+        w = ii()
+        c = (for _ <- 1..h, do: input())|> List.to_tuple()
+        solve(h,w,c)
     end
 
 end
